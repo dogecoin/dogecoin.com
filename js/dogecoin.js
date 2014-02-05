95b8c7447af022e6a05b6be00afdce23bb435daf
ummjackson <hello@ummjackson.com>
2014-02-05 17:27:29 +1100

    Update dogecoin.js

--- js/dogecoin.js
+++ js/dogecoin.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%20%3D%20'http%3A%2F%2Fcoinmarketcap.com%2F'%20and%20xpath%3D'%2F%2Fa%5Bcontains(%40href%2C%22http%3A%2F%2Fwww.cryptocoincharts.info%23jump-doge-btc%22)%5D'&format=json", function(data) {
+    $.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%20%3D%20'http%3A%2F%2Fcoinmarketcap.com%2F'%20and%20xpath%3D'%2F%2Fa%5Bcontains(%40href%2C%22%2Fvolume.html%23doge%22)%5D'&format=json", function(data) {
         price = (data.query.results.a).replace(/ /g, '');
         $('.price-box').append('<a href="http://www.cryptocoincharts.info/#jump-doge-btc" target="_blank">US' + price + '</a>');
     });
