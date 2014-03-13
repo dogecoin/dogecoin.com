85ae1a5f1f7386bbc3881a17dd138f223d179906
Jackson Palmer <hello@ummjackson.com>
2014-03-12 22:00:34 -0700

    Correcting price.

--- js/dogecoin.js
+++ js/dogecoin.js
@@ -18,10 +18,12 @@ $(document).ready(function() {
 
     });
 
+    /*
     $.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%20%3D%20'http%3A%2F%2Fcoinmarketcap.com%2F'%20and%20xpath%3D'%2F%2Fa%5Bcontains(%40href%2C%22%2Fvolume.html%23doge%22)%5D'&format=json", function(data) {
         price = (data.query.results.a[0]).replace(/ /g, '');
         $('.price-box').append('<a href="http://www.cryptocoincharts.info/#jump-doge-btc" target="_blank">US' + price + '</a>');
     });
+    */
 
     function addItem() {
         items = Array('such currency', 'wow', 'amaze', 'much coin', 'awesome', 'so crypto', 'how money', 'plz mine', 'v rich', 'many coins', 'such profit', 'WOW');
