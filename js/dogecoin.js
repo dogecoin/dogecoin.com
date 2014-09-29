f5af273f276c554df76df683af85d21ffeee8e77
David Pollack <david@da3.net>
2014-09-29 21:21:36 +0200

    initial internationalization commit with jquery.i18n with Block.io

--- js/dogecoin.js
+++ js/dogecoin.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
                 break;
         }
     }
+    
+    enablei18n();
 
     $('body').on('click', '.watch-video', function(){
         $('.logo-container').removeClass('logo').addClass('video').html('<iframe width="801" height="455" id="youtube-frame" src="http://www.youtube.com/embed/_KVZmS_UO5I?enablejsapi=1&autoplay=1" frameborder="0" allowfullscreen></iframe>').fitVids();
