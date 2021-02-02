67c4be89c0e4cf087ca5bd29f22458432de26b0f
langerhans <langerhans@users.noreply.github.com>
2021-02-02 20:04:18 +0100

    Video switcheroo

--- js/dogecoin.js
+++ js/dogecoin.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     enablei18n();
 
     $('body').on('click', '.watch-video', function(){
-        $('.logo-container').removeClass('logo').addClass('video').html('<iframe width="801" height="455" id="youtube-frame" src="http://www.youtube.com/embed/_KVZmS_UO5I?enablejsapi=1&autoplay=1" frameborder="0" allowfullscreen></iframe>').fitVids();
+        $('.logo-container').removeClass('logo').addClass('video').html('<iframe width="801" height="455" id="youtube-frame" src="https://www.youtube.com/embed/_KVZmS_UO5I?enablejsapi=1&autoplay=1" frameborder="0" allowfullscreen></iframe>').fitVids();
         new YT.Player('youtube-frame', {
                 events: {
                     'onStateChange': onPlayerStateChange
