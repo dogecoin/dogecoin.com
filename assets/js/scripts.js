d06ceb20892b5fae3a14564f1bce224def04ffe7
Emrullah SAKU <emrullah@sakusoft.com>
2021-05-10 01:23:17 +0300

    Dogecoin Turkish Languges translation for  Website
    Dogecoin Turkish Languges translation for  Website is completed. Please review and accept my request.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -205,7 +205,7 @@
     lang = setLang;
   }
 
-  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it'){
+  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it' && lang != 'tr'){
     lang = 'en';
   }
   sessionStorage.setItem('dgc-lang', lang);
