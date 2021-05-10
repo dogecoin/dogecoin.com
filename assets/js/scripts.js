4b523867dca694a9a1d6b41cbd52b42202698ec2
vj-codes <vj.codes29@gmail.com>
2021-05-11 00:38:10 +0530

    added marathi translation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -205,7 +205,7 @@
     lang = setLang;
   }
 
-  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it'){
+  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it' && lang !='mr'){
     lang = 'en';
   }
   sessionStorage.setItem('dgc-lang', lang);
