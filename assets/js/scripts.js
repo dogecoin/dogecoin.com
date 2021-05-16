73bbcbad29da7d51022b3c9ecf8f4bc8338fa402
Aditya Tyagi <77538183+adityagi02@users.noreply.github.com>
2021-05-16 18:04:29 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -205,7 +205,7 @@
     lang = setLang;
   }
 
-  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it' && lang != 'hi' && lang != 'id' && lang!='mr' && lang != 'tr'){
+  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it' && lang != 'hi' && lang != 'id' && lang!='mr' && lang != 'tr' && lang != 'bg'){
     lang = 'en';
   }
   sessionStorage.setItem('dgc-lang', lang);
