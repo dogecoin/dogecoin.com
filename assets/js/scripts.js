e647bdcd02d0e44da1ad3306130c7503c0de2b59
Ori Nahum <orinahum@Oris-iMac.local>
2021-06-12 21:17:27 +0300

    Add hebrew translation and rtl support

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,7 @@
     */
     (function($) {
     "use strict"; // Start of use strict
-    
+
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -19,7 +19,7 @@
         }
       }
     });
-    
+
     // Scroll to top button appear
     $(document).scroll(function() {
       var scrollDistance = $(this).scrollTop();
@@ -29,18 +29,18 @@
         $('.scroll-to-top').fadeOut();
       }
     });
-    
+
     // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function() {
       $('.navbar-collapse').collapse('hide');
     });
-    
+
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
       target: '#mainNav',
       offset: 80
     });
-    
+
     // Collapse Navbar
     var navbarCollapse = function() {
       if ($("#mainNav").offset().top > 100) {
@@ -53,7 +53,7 @@
     navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
-    
+
     // Floating label headings for the contact form
     $(function() {
       $("body").on("input propertychange", ".floating-label-form-group", function(e) {
@@ -64,7 +64,7 @@
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
-    
+
   })(jQuery); // End of use strict
 
   function initParticles(){
@@ -189,7 +189,7 @@
    if(setLang == false){
      var sessionLang = sessionStorage.getItem('dgc-lang');
      if(typeof sessionLang == "undefined" || sessionLang == null || sessionLang == "null" || sessionLang == ""){
-       var userLang = navigator.language || navigator.userLanguage; 
+       var userLang = navigator.language || navigator.userLanguage;
        if(typeof userLang == "undefined" || userLang == ""){
         userLang = 'en-EN';
       }
@@ -205,13 +205,13 @@
     lang = setLang;
   }
 
-  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it' && lang != 'hi' && lang != 'id' && lang!='mr' && lang != 'tr' && lang!='kn' && lang != 'ru'){
+  if(lang != 'en' && lang != 'de' && lang != 'fr' && lang != 'es' && lang != 'it' && lang != 'hi' && lang != 'id' && lang!='mr' && lang != 'tr' && lang!='kn' && lang != 'ru' && lang != 'he'){
     lang = 'en';
   }
   sessionStorage.setItem('dgc-lang', lang);
-    
+
   i18n.locale = lang;
-  
+
   i18n.load('assets/i18n', i18n.locale).done(
     function() {
      $('[data-i18n]').each(function(index) {
@@ -219,6 +219,12 @@
       $(this).html($.i18n($(this).attr('data-i18n')));
     } );
    } );
+
+  // Rtl languages support
+  if (lang === "he") {
+    $("html").attr("dir", "rtl");
+    $("p").css("text-align", "center");
+  }
 }
 // Enable debug
 $.i18n.debug = false;
@@ -227,7 +233,7 @@ $(document).ready(function(){
 
   enablei18n(false);
   initParticles();
-  
+
 
   $(document).on("click", ".wallet-button", function(){
     $(".wallet-guide-detail").hide();
@@ -238,4 +244,3 @@ $(document).ready(function(){
     $(".scroll-to-top a").trigger('click');
   });
 });
-
